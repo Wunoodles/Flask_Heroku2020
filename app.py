388e9eb865c07484f7ee8501e7b6cb66fa5794a9
MinYen <noodlesloves@gmail.com>
2020-05-27 04:42:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
@@ -40,20 +38,26 @@ def callback():
     return 'OK'
 
 def getNews():
-    """
-    建立一個抓最新消息的function
-    """
-    rss_url = 'http://feeds.feedburner.com/cnaFirstNews'
-    # 抓取資料
-    rss = feedparser.parse(rss_url)
-    # 抓取第一個文章標題
-    title = rss['entries'][0]['title']
-    # 抓取第一個文章標題
-    link = rss.entries[0]['link']
-    
-    tmp = title + ' ' +link
-    return tmp
+	"""
+	建立一個抓最新消息的function
+	"""
+	import requests
+	import re
+	from bs4 import BeautifulSoup
 
+	url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
+	r = requests.get(url)
+	reponse = r.text
+
+	url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
+
+	soup = BeautifulSoup(url_list[0])
+	url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
+	title = soup.text
+
+
+	tmp = title + ': ' +url
+	return tmp
 	
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
