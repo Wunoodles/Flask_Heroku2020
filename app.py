3545744078e2cb1f3d5498dbe8bc1134eb5a926d
MinYen <noodlesloves@gmail.com>
2020-05-27 12:31:26 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,14 +11,7 @@ from linebot.models import *
 
 app = Flask(__name__)
 
-# Channel access token (long-lived)
-line_bot_api = LineBotApi('73dYdWd9LTZ+z/nMDvbZVHIeHJKkhTQW8oFK1mUaHw8pgOclBx3EpbgM9vG9sQ4kkfUvLC53N3xZlfcmWobSOXCBVjnqsSyb+2OjzupXnwkQTpGTIbbx61k89TDnlY7Xf7o6H7+0kxmW9UNko6iXPAdB04t89/1O/w1cDnyilFU=')
 
-# Channel secret 
-handler = WebhookHandler('0e4384641b9c12109bcf4ebcb0e146b0')
-
-# User ID
-userID = 'U5da47e52a532692416abefc9655ca12c'
 
 def get_time():
 	from datetime import datetime 
